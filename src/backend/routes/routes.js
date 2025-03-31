73f227ec4e5fb6a9cd98e8a886989ad7c962373e
SANTOS Alexis <alexis.santos@algosup.com>
2025-03-31 16:29:51 +0200

    Second Push to Meet Technical Conventions
    Co-Authored-By: Lucas <145991204+LucasMegnan@users.noreply.github.com>

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('mainpage');
+    res.render('main');
 });
 
 router.get('/help', (req, res) => {
